3f009bf8a51b4472e9c8d391a46dc08f7b45c674
naimam <naimam@LIN51017928.corp.capgemini.com>
2021-11-20 13:02:44 +0530

    springrestassignments

--- demo/src/main/java/com/naved/demo/welcomeclass.java
+++ demo/src/main/java/com/naved/demo/welcomeclass.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.naved.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class welcomeclass {
 @GetMapping("/message")
 public String demomsg() {
-return "Hi How R U";
+return "Hello World";
 }
 
 
